8db0327a0d7c1d96798a9be0006de1d4cabfaee5
Irham Mustofa <93388477+irhamustofa@users.noreply.github.com>
2023-11-11 13:40:39 +0700

    wip: refactoring name and methods

--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -1,35 +1,38 @@
 class Point:
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX
-        self.coordY = coordY
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class Rectangle:
-    def __init__(self, starting_point, broad, high):
+    def __init__(self, starting_point, width, height):
         self.starting_point = starting_point
-        self.broad = broad
-        self.high = high
+        self.width = width
+        self.height = height
+        self.end_points(self)
 
     def area(self):
-        return self.broad * self.high
+        return self.width * self.height
 
     def end_points(self):
-        top_right = self.starting_point.coordX + self.broad
-        bottom_left = self.starting_point.coordY + self.high
-        print('Starting Point (X)): ' + str(self.starting_point.coordX))
-        print('Starting Point (Y)): ' + str(self.starting_point.coordY))
-        print('End Point X-Axis (Top Right): ' + str(top_right))
-        print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
+        end_point_x = self.starting_point.x + self.width
+        end_point_y = self.starting_point.y + self.height
+
+    def print_points(self):
+        print('Starting Point (X)): ' + str(self.starting_point.x))
+        print('Starting Point (Y)): ' + str(self.starting_point.y))
+        print('End Point X-Axis (Top Right): ' + str(end_point_x))
+        print('End Point Y-Axis (Bottom Left): ' + str(end_point_y))
 
 
-def build_stuff():
-    main_point = Point(50, 100)
-    rect = Rectangle(main_point, 90, 10)
+def create_rectangle():
+    starting_point = Point(50, 100)
+    rectange = Rectangle(starting_point, 90, 10)
 
-    return rect
+    return rectangle
 
 
-my_rect = build_stuff()
+my_rect = create_rectangle()
 
 print(my_rect.area())
 my_rect.end_points()
